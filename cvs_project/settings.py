4a45c30823db1133d7e042aaa13f7afcd4007422
David McCann <david.a.mccann@gmail.com>
2011-08-19 08:40:54 +0300

    changed naming convention

--- cvs_project/settings.py
+++ cvs_project/settings.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.join(filedir, 'rapidsms_xforms_src'))
 sys.path.append(os.path.join(filedir, 'rapidsms_healthmodels'))
 sys.path.append(os.path.join(filedir, 'rapidsms_tracking'))
 sys.path.append(os.path.join(filedir, 'django_eav'))
-sys.path.append(os.path.join(filedir, 'uganda_common'))
+sys.path.append(os.path.join(filedir, 'rapidsms_uganda_common'))
 sys.path.append(os.path.join(filedir, 'rapidsms_unregister'))
 
 # -------------------------------------------------------------------- #

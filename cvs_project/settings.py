f0b2bc6f04795b6f42b8f2b24a193dc957d4efd6
David McCann <david.a.mccann@gmail.com>
2011-09-28 12:12:55 +0300

    got rid of tracking

--- cvs_project/settings.py
+++ cvs_project/settings.py
@@ -22,7 +22,6 @@ sys.path.append(os.path.join(filedir, 'rapidsms_uregister'))
 sys.path.append(os.path.join(filedir, 'rapidsms_ureport'))
 sys.path.append(os.path.join(filedir, 'rapidsms_xforms_src'))
 sys.path.append(os.path.join(filedir, 'rapidsms_healthmodels'))
-sys.path.append(os.path.join(filedir, 'rapidsms_tracking'))
 sys.path.append(os.path.join(filedir, 'django_eav'))
 sys.path.append(os.path.join(filedir, 'rapidsms_uganda_common'))
 sys.path.append(os.path.join(filedir, 'rapidsms_uganda_ussd'))
@@ -111,7 +110,6 @@ INSTALLED_APPS = [
     "contact",
     "unregister",
     "ussd",
-    "tracking",
     "script",
 ]
 
